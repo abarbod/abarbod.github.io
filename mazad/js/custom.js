26cd5ff59030f2f981e5f4199f5553dcc8bdb189
Ahmed Barabbud <a.a.barbod@gmail.com>
2021-03-04 22:12:06 +0300

    update index

--- mazad/js/custom.js
+++ mazad/js/custom.js
@@ -25,9 +25,9 @@ $(document).ready(function(){
     });
 
     // ===========Featured Owl Carousel============
-    var objowlcarousel=$(".owl-carousel-featured");
-    if (objowlcarousel.length > 0) {
-        objowlcarousel.owlCarousel({
+    var objowlCarouselRtl=$(".owl-carousel-featured");
+    if (objowlCarouselRtl.length > 0) {
+        objowlCarouselRtl.owlCarouselRtlRtl({
             items: 4,
             lazyLoad: true,
             pagination: false,
@@ -35,14 +35,25 @@ $(document).ready(function(){
             navigation: true,
             navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
             stopOnHover: true,
-           
+            rtl: true,
+            center: true,
+            // items: 4,
+            // responsiveClass: true,
+            // loop: false,
+            // nav: false,
+            // margin: 10,
+            // lazyLoad: false,
+            // autoplay: false,
+            // autoplayTimeout: 10000,
+            // smartSpeed: 200,
+            autoWidth: true,
         });
     }
     
     // ===========Categories List Page============
     var mycategorylistpage=$(".categories-list-page");
     if (mycategorylistpage.length > 0) {
-        mycategorylistpage.owlCarousel({
+        mycategorylistpage.owlCarouselRtl({
             items: 3,
             lazyLoad: true,
             pagination: false,
@@ -104,68 +115,71 @@ $(document).ready(function(){
     $('#back-to-top').tooltip('hide');
     
     // ===========Single Ads Slider============	
-    // var sync1 = $("#sync1");
-    // var sync2 = $("#sync2");
-    //   sync1.owlCarousel({
-    //     singleItem : true,
-    //     slideSpeed : 1000,
-    //     pagination: false,
-    //     navigation: true,
-    //     navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    //     autoPlay: 1500,
-    //     afterAction : syncPosition,
-    //     responsiveRefreshRate : 200,
-    // });
-    // sync2.owlCarousel({
-    //     items : 4,
-    //     itemsDesktop      : [1199,10],
-    //     itemsDesktopSmall     : [979,10],
-    //     itemsTablet       : [768,8],
-    //     itemsMobile       : [479,4],
-    //     pagination:false,
-    //     responsiveRefreshRate : 100,
-    //     afterInit : function(el){
-    //       el.find(".owl-item").eq(0).addClass("synced");
-    //     }
-    // });
-    // function syncPosition(el){
-    //     var current = this.currentItem;
-    //     $("#sync2")
-    //       .find(".owl-item")
-    //       .removeClass("synced")
-    //       .eq(current)
-    //       .addClass("synced")
-    //     if($("#sync2").data("owlCarousel") !== undefined){
-    //       center(current)
-    //     }
-    // }
-    // $("#sync2").on("click", ".owl-item", function(e){
-    //     e.preventDefault();
-    //     var number = $(this).data("owlItem");
-    //     sync1.trigger("owl.goTo",number);
-    // });
-    // function center(number){
-    //     var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
-    //     var num = number;
-    //     var found = false;
-    //     for(var i in sync2visible){
-    //       if(num === sync2visible[i]){
-    //         var found = true;
-    //       }
-    //     }
-    //     if(found===false){
-    //       if(num>sync2visible[sync2visible.length-1]){
-    //         sync2.trigger("owl.goTo", num - sync2visible.length+2)
-    //       }else{
-    //         if(num - 1 === -1){
-    //           num = 0;
-    //         }
-    //         sync2.trigger("owl.goTo", num);
-    //       }
-    //     } else if(num === sync2visible[sync2visible.length-1]){
-    //       sync2.trigger("owl.goTo", sync2visible[1])
-    //     } else if(num === sync2visible[0]){
-    //       sync2.trigger("owl.goTo", num-1)
-    //     }
-    // }
+    var sync1 = $("#sync1");
+    var sync2 = $("#sync2");
+      sync1.owlCarouselRtl({
+        singleItem : true,
+        slideSpeed : 1000,
+        pagination: false,
+        navigation: true,
+        navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        autoPlay: 1500,
+        afterAction : syncPosition,
+        responsiveRefreshRate : 200,
+    });
+    sync2.owlCarouselRtl({
+        items : 4,
+        itemsDesktop      : [1199,10],
+        itemsDesktopSmall     : [979,10],
+        itemsTablet       : [768,8],
+        itemsMobile       : [479,4],
+        pagination:false,
+        responsiveRefreshRate : 100,
+        afterInit : function(el){
+          el.find(".owl-item").eq(0).addClass("synced");
+        }
+    });
+    function syncPosition(el){
+        var current = 0;
+        $("#sync2")
+          .find(".owl-item")
+          .removeClass("synced")
+          .eq(current)
+          .addClass("synced")
+        if($("#sync2").data("owlCarouselRtl") !== undefined){
+          center(current)
+        }
+    }
+    $("#sync2").on("click", ".owl-item", function(e){
+        e.preventDefault();
+        var number = $(this).data("owlItem");
+        sync1.trigger("owl.goTo",number);
+    });
+    function center(number){
+        var sync2visible = sync2.data("owlCarouselRtl").owl.visibleItems;
+        var num = number;
+        var found = false;
+        for(var i in sync2visible){
+          if(num === sync2visible[i]){
+            var found = true;
+          }
+        }
+        if(found===false){
+          if(num>sync2visible[sync2visible.length-1]){
+            sync2.trigger("owl.goTo", num - sync2visible.length+2)
+          }else{
+            if(num - 1 === -1){
+              num = 0;
+            }
+            sync2.trigger("owl.goTo", num);
+          }
+        } else if(num === sync2visible[sync2visible.length-1]){
+          sync2.trigger("owl.goTo", sync2visible[1])
+        } else if(num === sync2visible[0]){
+          sync2.trigger("owl.goTo", num-1)
+        }
+    }
+    var imported = document.createElement('script');imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-120909275-1';document.head.appendChild(imported);window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-120909275-1');
+
 });
